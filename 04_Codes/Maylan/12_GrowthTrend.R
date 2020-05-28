15ee8a818eaa4420cbe2fe1008b460f9d90f0c13
ruoning88 <34512085+ruoning88@users.noreply.github.com>
2020-05-27 19:04:35 -0500

    Update 12_GrowthTrend.R

--- 04_Codes/Maylan/12_GrowthTrend.R
+++ 04_Codes/Maylan/12_GrowthTrend.R
@@ -5,14 +5,13 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 ## Test Code P44/45 Growth Trend
 
-
 GrowthTrend <- function(data,
                         form,
                         page,
                         digit,
                         directory) {
   
-  table.file <- data %>% 
+table.file <- data %>% 
     mutate(summary = ifelse(!!sym(unique(form$Summary1)) %in% unique(form$Display), 
                             !!sym(unique(form$Summary1)), 
                             "Others")) %>% 
@@ -21,7 +20,7 @@ GrowthTrend <- function(data,
     summarise(Value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE)) %>%
     ungroup() %>%
     mutate (Value= Value/digit) %>%
-    filter(period %in% tail(unique(period),36)) %>%
+    filter(period %in% tail(sort(unique(period)),min(36,length(unique(period))))) %>%
     spread(period,Value) %>%
     adorn_totals("row", na.rm = TRUE, name = "Total") %>% 
     gather(period,Value,-summary) %>%
@@ -34,14 +33,53 @@ GrowthTrend <- function(data,
     select(summary,period,Growth) %>%
     spread(period,Growth) %>%
     mutate(sequence = ifelse(summary == 'Others' , 
-                             2, ifelse(summary=='Total',3,1))) %>%
+                            2, ifelse(summary=='Total',3,1))) %>%
     arrange(sequence,summary) %>%
     select(-sequence) %>%
-    right_join(distinct(form, Display), by = c("summary" = "Display")) %>% 
-    rename(` ` = summary)
-  
-  table.file
-  write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
+    rename(' ' = summary) 
+
+ref.table <- data.frame(period=colnames(table.file[-1])) %>%
+             mutate(month=str_sub(period,-2,-1),year=str_sub(period,1,2))
+timer <- 24-nrow(ref.table)
+newdf <- data.frame(month=rep(NA,timer),year=rep(NA,timer))
+for (i in 1:timer) {
+    mthindex <- as.numeric(ref.table$month[nrow(ref.table)])+i
+    for (d in 1:2){
+    if (mthindex > 12) {
+      mthindex <- mthindex - 12
+    }}
+    mthindex <- str_pad(mthindex, 2, pad = "0")
+    newdf$month[i] <- mthindex
+}
+
+if (newdf$month[timer]=='12'){
+  yrindex <- as.numeric(ref.table$year[1])-1
+} else {
+  yrindex <- as.numeric(ref.table$year[1])
+}
+
+for (i in 1:timer) {
+  key <- timer + 1 - i
+  mthindex <- as.numeric(newdf$month[key])
+  if(key == timer){
+    diff <- 1
+  } else {
+    secindex <- as.numeric(newdf$month[key+1])
+    diff <- secindex - mthindex
+  }
+  if (diff != 1) {
+    yrindex <- yrindex -1
+  }
+  newdf$year[key] <- yrindex
+}
+newdf <- newdf %>% mutate(period=paste0(year,str_sub(ref.table$period[1],-3,-3),month))
+ncolnm <- c(newdf$period)
+table2 <- data.frame(matrix(nrow=nrow(table.file),ncol=length(ncolnm))) 
+colnames(table2) <- ncolnm
+table.file <- cbind(table.file[,1],table2,table.file[,2:ncol(table.file)])
+
+table.file
+write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
 }
 
 
