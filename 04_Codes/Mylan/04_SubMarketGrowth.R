fa1d785f67f22d195742059df4ad01cdf809bea7
ruoning88 <34512085+ruoning88@users.noreply.github.com>
2020-05-29 20:29:53 -0500

    5 Years Continuous Display

--- 04_Codes/Mylan/04_SubMarketGrowth.R
+++ 04_Codes/Mylan/04_SubMarketGrowth.R
@@ -32,6 +32,34 @@ SubMarketGrowth <- function(data,
     right_join(distinct(form, Display), by = c("sub_market" = "Display")) %>% 
     rename("Growth%" = sub_market)
   
+  pdnm <- colnames(table.file[-1])
+  if (length(grep("^(?=\\bNA\\b).*",pdnm,value = TRUE, perl = TRUE)) != 0) {
+    pdnm <- pdnm[pdnm != grep("^(?=\\bNA\\b).*",pdnm,value = TRUE, perl = TRUE)]
+  }
+  
+  countpd <- length(pdnm)
+  if (countpd<5) {
+    timer <- 5 - countpd
+    nleng <- nchar(pdnm[1])
+    ref.table <- data.frame(PD=pdnm) %>% mutate(Year=str_sub(PD,1,2),Month=str_sub(PD,3,nleng))
+    newdf <- data.frame(month=rep(unique(ref.table$Month),timer),year=rep(NA,timer))
+    yrindex <- as.numeric(ref.table$Year[1])
+    for (i in 1:timer) {
+      yrindex <- yrindex -1
+      newdf$year[timer+1-i] <- yrindex
+    }
+    newdf <- newdf %>% mutate(pd=paste0(year,month)) 
+    ncolnm <- length(unique(newdf$pd))
+    adddf <- data.frame(matrix(nrow=nrow(table.file),ncol=ncolnm)) 
+    colnames(adddf) <- unique(newdf$pd)
+    table.file <- cbind(table.file[1],adddf,table.file[-1])
+    
+    cdnm <- colnames(table.file[-1])
+    if (length(grep("^(?=\\bNA\\b).*",cdnm,value = TRUE, perl = TRUE)) != 0) {
+      table.file <- table.file[,-ncol(table.file)]
+    }
+  }
+  
   table.file
   write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
 }
