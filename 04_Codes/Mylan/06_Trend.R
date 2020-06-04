bb907c3c182ff0a1775d896bbc22d81801ca36d8
ruoning88 <34512085+ruoning88@users.noreply.github.com>
2020-06-03 21:36:36 -0500

    Update 06_Trend.R

--- 04_Codes/Mylan/06_Trend.R
+++ 04_Codes/Mylan/06_Trend.R
@@ -5,30 +5,73 @@
 # Date:         2020-05-14
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-
 Trend <- function(data,
                   form,
                   page,
                   digit,
                   directory) {
   
+  if(unique(form$Period)=='MTH') {
+     grouper <- 'MTH' } else {
+     grouper <- 'MAT'  
+     }
+    
   table.file <- data %>% 
-    filter(!!sym(unique(form$Period)) %in% 
-             tail(sort(unique(unlist(data[, unique(form$Period)]))), 24)) %>% 
+    filter(!!sym(grouper) %in% 
+             tail(sort(unique(unlist(data[, grouper]))), 
+                  min(24,length(unique(unlist(data[, grouper])))))) %>% 
     mutate(summary = ifelse(!!sym(unique(form$Summary1)) %in% unique(form$Display), 
                             !!sym(unique(form$Summary1)), 
-                            "Others")) %>% 
-    group_by(period = !!sym(unique(form$Period)), summary) %>% 
-    summarise(value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE) / digit) %>% 
-    ungroup() %>% 
-    arrange(period) %>% 
-    setDT() %>% 
-    dcast(summary ~ period, value.var = "value") %>% 
-    right_join(distinct(form, Display), by = c("summary" = "Display")) %>% 
-    rename(` ` = summary)
+                            "Others"))  
+    
+   if (unique(form$Period)=='MTH') {
+    table.file <- table.file %>%
+      group_by(period = !!sym(unique(form$Period)), summary) %>% 
+      summarise(value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE) / digit) %>% 
+      ungroup() %>% 
+      arrange(period) %>% 
+      setDT() %>% 
+      dcast(summary ~ period, value.var = "value") %>% 
+      right_join(distinct(form, Display), by = c("summary" = "Display")) %>% 
+      rename(` ` = summary)
+    
+   } else  {
+     
+    if (unique(form$Period)=='MAT') {
+       rollparam <- 12
+     } else {
+       rollparam <- 3
+     }
+     
+     table.file <- table.file %>%
+       group_by(PeriodMAT = !!sym(grouper),
+                PeriodMTH = MTH,
+                summary) %>%
+       summarise(value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE) / digit) %>% 
+       ungroup() %>%
+       filter(PeriodMTH %in% tail(sort(unique(PeriodMTH)),min(36,length(unique(PeriodMTH))))) %>%
+       arrange(summary,PeriodMAT, PeriodMTH) %>%
+       group_by(summary) %>%
+       mutate(RollValue=c(rep(NA,(rollparam-1)),rollsum(value,rollparam))) %>%
+       ungroup() %>% na.omit() %>%
+       filter(PeriodMTH %in% tail(sort(unique(PeriodMTH)),min(24,length(unique(PeriodMTH))))) %>% 
+       mutate(period = paste(PeriodMTH,unique(form$Period),sep=' ')) %>%
+       select(period,summary, RollValue,-value,-PeriodMAT,-PeriodMTH) %>%
+       spread(period,RollValue) %>%
+       adorn_totals("row", na.rm = TRUE, name = "Total") %>%
+       mutate(sequence = ifelse(summary == 'Others' , 
+                                2, ifelse(summary=='Total',3,1))) %>%
+       arrange(sequence,summary) %>%
+       select(-sequence) %>%
+       rename(' ' = summary) 
+       
+       source("04_Codes/Mylan/14_DisplayFunction.R", encoding = "UTF-8")
+       table.file <- DisplayFunction(table.file=table.file)
+   }
   
   table.file
   write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
 }
+
 
 
