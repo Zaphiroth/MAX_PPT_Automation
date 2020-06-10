e996074aedbc1a1f0a123b17d7e7f3d41f74e396
Zaphiroth <zliu@pharbers.com>
2020-06-10 11:46:34 +0800

    Delete source

--- 06_Deliveries/PPTAutomation/inst/Mylan/function/RegionTrend.R
+++ 06_Deliveries/PPTAutomation/inst/Mylan/function/RegionTrend.R
@@ -43,7 +43,6 @@ RegionTrend <- function(data,
       select(Name, everything(),-category,-region) %>%
       rename(' ' = Name)
 
-    source("04_Codes/Mylan/14_DisplayFunction.R", encoding = "UTF-8")
     table.file <- DisplayFunction(table.file=table.file, type='MTH')
     write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
 
@@ -91,7 +90,6 @@ RegionTrend <- function(data,
     }
 
     if (nrow(table.file) != 0) {
-      source("04_Codes/Mylan/14_DisplayFunction.R", encoding = "UTF-8")
       table.file <- DisplayFunction(table.file=table.file,type=unique(form$Period))
       write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
     } else {
