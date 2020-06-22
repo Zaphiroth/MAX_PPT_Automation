f3ab95809bfbf5b5b2d64f583e2f8c9f6b030414
ruoning88 <34512085+ruoning88@users.noreply.github.com>
2020-06-22 16:58:31 -0500

    Quarter

---
+++ 04_Codes/Mylan/11_CompetitorPerformanceQ.R
@@ -0,0 +1,73 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ProjectName:  MAX PPT Automation
+# Purpose:      Maylan PPT Function
+# Date:         2020-06-18
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+CompetitorPerformance <- function(data,
+                                  form,
+                                  page,
+                                  digit,
+                                  directory) {
+  
+  dataQ <- data %>% mutate(MATY=str_sub(data$MAT,1,2), YTDY=str_sub(data$YTD,1,2), QT=as.numeric(str_sub(data$Date,5,6))/3) %>%
+    mutate(MAT = paste0(MATY,'Q4 MAT'), YTD=paste0(YTDY,'Q4 YTD'), MTH=paste0(MATY,'Q',QT)) %>%
+    select(-MATY, -YTDY, -QT)
+  
+  table1 <- dataQ %>% 
+    group_by(period = !!sym(unique(form$Period)),
+             region = !!sym(unique(form$Summary1)),
+             product = !!sym(unique(form$Summary2))) %>% 
+    summarise(sub_value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    filter(period %in% tail(unique(period),2) & product %in% unique(form$Internal)) %>% 
+    mutate(type = product, sequence = 0) %>% 
+    select(type,period,region,sub_value,sequence) 
+  
+  
+  table2 <- dataQ %>% 
+    filter (!!sym(unique(form$Summary2)) != unique(form$Internal) ) %>%
+    mutate(type = ifelse(!!sym(unique(form$Summary2)) %in% unique(form$Display), 
+                         !!sym(unique(form$Summary2)), 
+                         "Others")) %>%
+    group_by(period = !!sym(unique(form$Period)),
+             region = !!sym(unique(form$Summary1)),
+             type ) %>%
+    summarise(sub_value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE)) %>%
+    ungroup() %>% 
+    filter(period %in% tail(unique(period),2)) %>%
+    mutate (sequence=ifelse(type=='Others',2,1),
+            type = factor(type, levels = c(unique(form$Display),'Others')))%>%
+    select(type,period,region,sub_value,sequence)  
+  
+  
+  table.file <- table1 %>% rbind(table2) %>% 
+    group_by(period,region) %>%
+    mutate(total = sum(sub_value)) %>%
+    ungroup() %>% 
+    mutate(Share = (sub_value/total)*100) %>%
+    arrange(type,region) %>%
+    group_by(type,region) %>%
+    mutate (EI=(Share / lag(Share))*100) %>% na.omit() %>%
+    ungroup() %>%
+    select(type,region,Share,EI,sequence) %>%
+    gather (category,value,Share:EI) %>%
+    spread(region,value) %>%
+    mutate (Name = paste(category,"_",type)) %>%
+    arrange(desc(category),sequence) %>%
+    select (Name, everything(),-type,-category,-sequence) %>% 
+    gather (region, value,-Name) %>%
+    spread(Name,value) 
+  
+  colnm <- colnames(table.file[,-1])
+  table.file <- table.file[,c('region',grep("^Share(?!.*Others)",colnm,value = TRUE, perl = TRUE),
+                              grep("^Share.*(?=.*\\bOthers\\b)",colnm,value=TRUE,perl=TRUE),
+                              grep("^EI(?!.*Others)",colnm,value = TRUE, perl = TRUE),
+                              grep("^EI.*(?=.*\\bOthers\\b)",colnm,value=TRUE,perl=TRUE))] 
+  table.file <- table.file %>% rename(" " = region)
+  
+  table.file
+  write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
+}
+
