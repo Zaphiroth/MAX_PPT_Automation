af1ea9d4d8b09b9b6bfa10de997e75b929a9190a
ruoning88 <34512085+ruoning88@users.noreply.github.com>
2020-07-13 01:36:03 -0500

    Quarter-MTH

--- 04_Codes/Mylan/03_SubMarketShareQ.R
+++ 04_Codes/Mylan/03_SubMarketShareQ.R
@@ -11,7 +11,8 @@ SubMarketShare <- function(data,
                            digit,
                            directory) {
   
- dateformat <- data.frame(Date=sort(unique(data$Date)),Quarter=rep(NA,length(unique(data$Date))),
+### Data Function
+  dateformat <- data.frame(Date=sort(unique(data$Date)),Quarter=rep(NA,length(unique(data$Date))),
                            Year=rep(NA,length(unique(data$Date))))
   dateformat$Quarter <- as.numeric(substr(dateformat$Date[nrow(dateformat)],5,6))/3
   timer <- nrow(dateformat)
@@ -29,56 +30,101 @@ SubMarketShare <- function(data,
     mutate(MAT = paste0(Year,'Q', Quarter,' MAT'), YTD=paste0(Year,'Q', Quarter,' YTD')) %>%
     select(-Quarter, -Year)
   
-  table.file <- dataQ %>% 
+
+### Display Function 
+if (unique(form$Period)=='MTH') {  
+  cy <- 4
+  last.date <- last(sort(unique(dataQ$Date)))
+  last.year <- as.numeric(stri_sub(last.date, 1, 4))  
+  last.mth <- as.numeric(stri_sub(last.date, 5, 6))
+  if(last.mth == 12) { tvar <-4 } else {tvar <-5 }
+  display.year <- stri_sub((last.year-tvar):last.year, 3, 4)
+  
+  first.mth <- last.mth + (12/cy) 
+  if(first.mth > 12) {first.mth <- first.mth -12}
+  fdmth <- seq(first.mth,12,by=(12/cy))
+  ldmth <- seq(3,last.mth,by=(12/cy))
+  rowtimer <- length(fdmth) + length(ldmth) + (length(display.year)-2)*cy
+  if(last.mth == 12) {
+    displayyr <- sort(c(rep(display.year,cy)))} else {
+      displayyr <- c(rep(display.year[1],length(fdmth)),rep(display.year[2],cy),
+                     rep(display.year[3],cy),rep(display.year[4],cy),rep(display.year[5],cy),
+                     rep(display.year[6],length(ldmth)))                 
+    }
+  displaymth <- as.character(c(fdmth,rep(seq(3,12,by=(12/cy)),length(display.year)-2),ldmth))
+  displaydf <- data.frame(year=displayyr,mth=displaymth) %>%
+    mutate(mth=as.numeric(mth)/3) %>%
+    mutate(period=paste0(year,"Q",mth))
+  display <- data.frame(period=displaydf$period)  
+  
+} else {
+  
+  display <- data.frame(period = seq(max(as.numeric(dataQ$Date)), as.numeric(max(dataQ$Date))-400, -100)) %>% 
+             arrange(period) %>% 
+             mutate(period = paste0(stri_sub(period, 3, 4), "Q", as.numeric(stri_sub(period, 5, 6))/3 , " ", unique(form$Period))) 
+}
+  
+display <- display %>%  
+    mutate (period_index = paste0(period, " Share")) %>% 
+    setDT() %>% 
+    melt(measure.vars = c("period", "period_index"), value.name = "period_index") %>% 
+    select(period_index) %>% 
+    setDF() %>% 
+    merge(distinct(form, sub_market = Display))  
+  
+
+### Main Table 
+table.file <- dataQ %>% 
     group_by(period = !!sym(unique(form$Period)),
-             sub_market = !!sym(unique(form$Summary1))) %>% 
+           sub_market = !!sym(unique(form$Summary1))) %>% 
     summarise(value = sum(!!sym(unique(form$Calculation)), na.rm = TRUE) / digit) %>% 
     ungroup() %>% 
+    filter(!is.na(period)) %>% 
     group_by(period) %>% 
     mutate(share = value / sum(value, na.rm = TRUE)) %>% 
     ungroup() %>% 
     arrange(period) %>% 
     setDT() %>% 
     melt(id.vars = c("period", "sub_market"), variable.factor = FALSE, 
-         variable.name = "index", value.name = "value") %>% 
-    mutate(index = ifelse(index == "value", "", "Share")) %>% 
+       variable.name = "index", value.name = "value") %>% 
+    mutate(index = ifelse(index == "value", "", "Share"))
+
+if (unique(form$Period)=='MTH') {
+  table.file <- table.file %>%  
+    mutate(month=str_sub(period,4,5),year=str_sub(period,1,2)) %>%
+    mutate(period = paste0(year,'Q', as.numeric(month)/3)) %>%
+    select(-year,-month) }
+
+table.file <- table.file %>%
     unite(period_index, period, index, sep = " ") %>% 
+    mutate(period_index = trimws(period_index)) %>% 
+    right_join(display, by = c("period_index", "sub_market")) %>% 
     setDT() %>% 
-    dcast(sub_market ~ period_index, value.var = "value") %>% 
+    dcast(sub_market ~ period_index, value.var = "value") 
+
+
+if (unique(form$Period)=='MTH') {
+  NameList <- grep(pattern='.[0-9]$',x=colnames(table.file),value=TRUE)
+  colnum <- which(colnames(table.file) %in% NameList)
+  
+  table.file <- table.file %>%  
+    select(sub_market,
+           colnum,
+           ends_with("Share")) %>% 
+    right_join(distinct(form, Display), by = c("sub_market" = "Display"))
+  
+} else {
+  
+  table.file <- table.file %>%  
     select(sub_market,
            ends_with(unique(form$Period)),
            ends_with("Share")) %>% 
     right_join(distinct(form, Display), by = c("sub_market" = "Display"))
-  
-  
-  pdnm <- colnames(table.file[-1])
-  if (length(grep("^(?=\\bNA\\b).*",pdnm,value = TRUE, perl = TRUE)) != 0) {
-    pdnm <- pdnm[pdnm != grep("^(?=\\bNA\\b).*",pdnm,value = TRUE, perl = TRUE)]
-  }
-  
-  countpd <- length(pdnm)
-  if (countpd<5) {
-    timer <- 5 - countpd
-    nleng <- nchar(pdnm[1])
-    ref.table <- data.frame(PD=pdnm) %>% mutate(Year=str_sub(PD,1,2),Month=str_sub(PD,3,nleng))
-    newdf <- data.frame(month=rep(unique(ref.table$Month),timer),year=rep(NA,timer))
-    yrindex <- as.numeric(ref.table$Year[1])
-    for (i in 1:timer) {
-      yrindex <- yrindex -1
-      newdf$year[timer+1-i] <- yrindex
-    }
-    newdf <- newdf %>% mutate(pd=paste0(year,month)) 
-    ncolnm <- length(unique(newdf$pd))
-    adddf <- data.frame(matrix(nrow=nrow(table.file),ncol=ncolnm)) 
-    colnames(adddf) <- unique(newdf$pd)
-    table.file <- cbind(table.file[1],adddf,table.file[-1])
-    
-    cdnm <- colnames(table.file[-1])
-    if (length(grep("^(?=\\bNA\\b).*",cdnm,value = TRUE, perl = TRUE)) != 0) {
-      table.file <- table.file[,-ncol(table.file)]
-    }
-  }
-  
+}
+
+   
+
   table.file
   write.xlsx(table.file,paste0(directory,'/',page,'.xlsx'))
 }
+
